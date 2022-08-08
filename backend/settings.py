0615c22e1387f9084c7156c847733dc41f4f4a6c
Juan Diego Garcia <juandiego0827@gmail.com>
2022-08-08 03:30:47 -0500

    fix: server

--- backend/settings.py
+++ backend/settings.py
@@ -172,3 +172,17 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'IBS2oAuC-boM2Fgtl2ab8fdv5bU',
     'SECURE': True,
 }
+
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
